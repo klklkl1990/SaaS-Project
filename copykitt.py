269502a3f4e4c83fe770d4b227904f8a17d4c06f
klklkl1990 <liadsolomon@hotmail.com>
2022-12-12 21:41:09 +0200

    added testing elements

--- copykitt.py
+++ copykitt.py
@@ -5,3 +5,4 @@ import openai
 openai.api_key = os.getenv("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")
 response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
 print(response)
+print(response["choices"][0]["text"])
