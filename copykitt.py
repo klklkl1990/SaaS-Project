2a9f0bbd5a6cee97a75e080b446ce3c3257b6059
klklkl1990 <liadsolomon@hotmail.com>
2022-12-12 23:50:31 +0200

    Fixed the env variable issue

--- copykitt.py
+++ copykitt.py
@@ -1,8 +1,8 @@
 import os
 import openai
-
+from consts import *
 # Load your API key from an environment variable or secret management service
-openai.api_key = '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'
+openai.api_key = OPEN_AI_KEY
 
 response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
 print(response)
