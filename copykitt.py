f35106d3b12e7ff8d66ecf8361db9a94fa48cb5c
klklkl1990 <liadsolomon@hotmail.com>
2022-12-12 22:41:50 +0200

    Added the OpenAI Key

--- copykitt.py
+++ copykitt.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv('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')
-response = openai.Completion.create(model='text-davinci-003', prompt='Say this is a test', temperature=0, max_tokens=7)
+openai.api_key = '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'
+
+response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
 print(response)
-print(response['choices'][0]['text'])
