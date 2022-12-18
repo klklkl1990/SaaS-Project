c358dfd839870aca6c05849af361f567b06dcffa
klklkl1990 <liadsolomon@hotmail.com>
2022-12-18 12:27:27 +0200

    trying to fix deploy issue

--- copykitt-infra/lib/copykitt-infra-stack.ts
+++ copykitt-infra/lib/copykitt-infra-stack.ts
@@ -9,15 +9,16 @@ export class CopykittInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const layer=new lambda.LayerVersion(this,"BaseLayer",{code:lambda.Code.fromAsset("lambda_base/layer.zip"), compatibleRuntimes:[lambda.Runtime.PYTHON_3_9],})
     // The code that defines your stack goes here
     const APILambda= new lambda.Function(
       this,"APIFunc",{
         runtime:lambda.Runtime.PYTHON_3_9,
         //code: lambda.Code.fromAsset("../SaaS-Project",)
         code: lambda.Code.fromAsset("../app",),
-        handler:"copykitt_api.handler"
+        handler:"copykitt_api.handler",
+        layers:[layer],
       })
-      
     }
     // example resource
     // const queue = new sqs.Queue(this, 'CopykittInfraQueue', {
